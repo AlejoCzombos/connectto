a11b104bcb10a7d0fb325cbe17e45087b7c9ea1d
Alejo <alejoczombos@gmail.com>
2024-06-03 22:55:39 -0300

    carrousel, Hero, navegacion y Header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+    './src/**/*.{astro,js,jsx,ts,tsx,vue}',
+    './public/index.html',
+  ],
 	theme: {
 		extend: {
 			colors: {
