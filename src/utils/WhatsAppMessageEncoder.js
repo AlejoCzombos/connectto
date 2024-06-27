d3cdb6234167be4df20e106b368f904895d6968a
Alejo <alejoczombos@gmail.com>
2024-06-27 10:35:55 -0300

    favicon y metadata

--- src/utils/WhatsAppMessageEncoder.js
+++ src/utils/WhatsAppMessageEncoder.js
@@ -2,6 +2,6 @@ import json from "../data/footer.json"
 
 export default function WhatsAppMessageEncoder(message) {
     const encodedMessage = encodeURIComponent(message); 
-    const url = `https://wa.me/?phone=549${json.contact.WhatsApp}&text=${encodedMessage}&type=phone_number`;
+    const url = `https://api.whatsapp.com/send/?phone=549${json.contact.WhatsApp}&text=${encodedMessage}&type=phone_number&app_absent=0`;
     return url
 }
